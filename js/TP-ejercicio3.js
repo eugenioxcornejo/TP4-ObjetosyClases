f93a854bbcc91bba3d7928234e454eb6f187cded
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-16 15:40:20 -0300

    problemas con git

--- js/TP-ejercicio3.js
+++ js/TP-ejercicio3.js
@@ -4,5 +4,25 @@ class Rectangulo {
         ancho,
     )
 
-    
+    this.largo = largoparam;
+    this.ancho = anchoparam;
+
+    cambiarlargo(largoparam){
+        document.write(`<h2>El Largo es ${this.largo}</h2>`);
+    }
+    cambiarancho(anchoparam){
+        document.write(`<h2>EL ancho es</h2>`);
+    }
+
+    perimetro(){
+        perimetro=(2*(this.ancho+this.largo));
+        document.write(`<h2>Elperimetro es ${perimetro}</h2>`);
+
+    }
+
+    area (){
+        area= this.largo*this.ancho;
+        document.write(`<h2>El area es ${area}</h2>`);
+        
+    }
 }
