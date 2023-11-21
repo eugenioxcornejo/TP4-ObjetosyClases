e2396a7e79ed5cf9eb729e69ef97dface0cf5fa6
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-21 16:12:06 -0300

    ejercicio 7 a medias

--- js/TP4-ejercicio7.js
+++ js/TP4-ejercicio7.js
@@ -1,13 +1,13 @@
 class agenda {
     #nombre;
-    #telefono;
+    
     constructor(
-        nombresparam = ['nombre'],
-        telefonosparam = ['telefono'],
+        nombresparam = [],
+        telefonosparam = [],
     )
 {
     this.#nombre = nombresparam;
-    this.#telefono = telefonosparam;
+    this.telefono = telefonosparam;
 
 }
 
@@ -16,25 +16,52 @@ get nombre () {
 }
 set nombre (nuevoNombre) {
     if (nuevoNombre.length <=10) {
-        
-    }
+        this.#nombre = nuevoNombre;
+        } else{
+            alert('la agenda esta ompleta');
+        }
 }
-aniadirContacto() {
+agendafuncion() {
+do {
+    let opcion = prompt ('Seleccione una opcion: 1-anadir un contacto 2-existe contacto? 3- listar contactos 4-buscar contacto 5-eliminar contacto  6-agenda llena? 7-cantidad de espacios libres')
+console.log(opcion.toLocaleLowerCase());
+    opcion= opcion.toLocaleLowerCase();
+
+
+switch(opcion){
+case '1' :
+    
+    do {
+        let i=1;
+    i=i+1;
     const nombre = prompt(`ingrese el nombre del contacto`);
     const telefono = prompt(`ingrese el telefono del contacto`);
-    this.nombre.push(nombre);
+    this.#nombre.push(nombre);
     this.telefono.push(telefono);
-    document.write(`Nombre: ${this.nombre}`);
-    document.write(`Telefono: ${this.telefono}`);
+    document.write(`<p/>Nombre: ${this.nombre[i]}</p>`);
+    document.write(`<p>Telefono: ${this.telefono[i]}</p>`);
+    }
+    while(confirm('desea anadir otro contacto?'))
+    break;
+    
     
 }
-
-
-
-
-
 }
+while (confirm('desea seleccionar otra opcion?'));
+
+
+}}
+
+
+ 
+
+
+
+
+
+
 
 const nuevocontacto = new agenda;
-nuevocontacto.aniadirContacto()
-console.log(nuevocontacto.aniadirContacto)
+nuevocontacto.agendafuncion()
+
+
